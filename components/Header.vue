83d0b931e6769226873fbfaea5d5074918b45fe0
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-10-16 09:23:00 +1100

    Re-enabled closing the window and some more renaming

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,24 @@
 <template>
   <div
-    @mousedown="() => window.startDragging()"
-    class="h-16 cursor-pointer bg-zinc-950 flex flex-row justify-between"
+      class="h-16 cursor-pointer bg-zinc-950 flex flex-row justify-between"
   >
-    <div class="flex flex-row grow items-center justify-between pl-5 pr-2 py-3">
+    <div @mousedown="() => window.startDragging()"
+         class="flex flex-row grow items-center justify-between pl-5 pr-2 py-3">
       <div class="inline-flex items-center gap-x-10">
         <NuxtLink to="/">
-          <Wordmark class="h-8 mb-0.5" />
+          <Wordmark class="h-8 mb-0.5"/>
         </NuxtLink>
         <nav class="inline-flex items-center mt-0.5">
           <ol class="inline-flex items-center gap-x-6">
             <NuxtLink
-              v-for="(nav, navIdx) in navigation"
-              :class="[
+                v-for="(nav, navIdx) in navigation"
+                :class="[
                 'transition  uppercase font-display font-semibold text-md',
                 navIdx === currentPageIndex
                   ? 'text-zinc-100'
                   : 'text-zinc-400 hover:text-zinc-200',
               ]"
-              :href="nav.route"
+                :href="nav.route"
             >
               {{ nav.label }}
             </NuxtLink>
@@ -29,25 +29,25 @@
         <ol class="inline-flex gap-3">
           <li v-for="(item, itemIdx) in quickActions">
             <HeaderWidget
-              @click="item.action"
-              :notifications="item.notifications"
+                @click="item.action"
+                :notifications="item.notifications"
             >
-              <component class="h-5" :is="item.icon" />
+              <component class="h-5" :is="item.icon"/>
             </HeaderWidget>
           </li>
-          <HeaderUserWidget />
+          <HeaderUserWidget/>
         </ol>
       </div>
     </div>
-    <WindowControl class="h-16 w-16 p-4" />
+    <WindowControl class="h-16 w-16 p-4"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import { BellIcon, UserGroupIcon } from "@heroicons/vue/16/solid";
-import type { NavigationItem, QuickActionNav } from "../types";
+import {BellIcon, UserGroupIcon} from "@heroicons/vue/16/solid";
+import type {NavigationItem, QuickActionNav} from "../types";
 import HeaderWidget from "./HeaderWidget.vue";
-import { getCurrentWindow } from "@tauri-apps/api/window";
+import {getCurrentWindow} from "@tauri-apps/api/window";
 
 const window = getCurrentWindow();
 
@@ -79,11 +79,13 @@ const currentPageIndex = useCurrentNavigationIndex(navigation);
 const quickActions: Array<QuickActionNav> = [
   {
     icon: UserGroupIcon,
-    action: async () => {},
+    action: async () => {
+    },
   },
   {
     icon: BellIcon,
-    action: async () => {},
+    action: async () => {
+    },
   },
 ];
 </script>
