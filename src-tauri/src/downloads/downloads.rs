8e9216babcd6a5dbd04081136717bc2b2ee17bd3
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-10-18 07:45:09 +1100

    Progress on downloads. Currently working on parsing functions to be run asynchronously

---
+++ src-tauri/src/downloads/downloads.rs
@@ -0,0 +1,22 @@
+/* GENERAL OUTLINE
+When downloading any game, the following details must be provided to the server:
+ - Game ID
+ - User token
+ - TBC
+
+The steps to then download a game are as follows:
+ 1. User requests  
+ */
+use tauri::AppHandle;
+use crate::auth::generate_authorization_header;
+use crate::DB;
+use crate::db::DatabaseImpls;
+use crate::downloads::manifest::Manifest;
+
+#[tauri::command]
+fn download_game(app: AppHandle, game_id: String) -> Result<String, String>{
+    todo!()
+}
+
+
+
