07379b2ba875f5ad0bdc5e5fdbd4157ae5de1c6d
quexeky <git@quexeky.dev>
2024-10-19 22:17:43 +1100

    SLowly integrating game_download into the FE. Started with using the manifest minimal example in the server (#1)

--- pages/store/index.vue
+++ pages/store/index.vue
@@ -1,3 +1,26 @@
 <template>
-    
+
+  <button class="w-full rounded-md p-4 bg-blue-600 text-white" @click="requestGameWrapper">
+    Load Data
+  </button>
 </template>
+<script setup lang="ts">
+definePageMeta({
+  layout: "mini",
+});
+
+import { invoke } from "@tauri-apps/api/core";
+
+async function requestGame() {
+  console.log("Requested game from FE");
+  await invoke("start_game_download", { gameId: "123", gameVersion: "1.2.3", maxThreads: 4 });
+}
+function requestGameWrapper() {
+  console.log("Wrapper started");
+  requestGame()
+      .then(() => {})
+      .catch((e) => {
+        console.log(e);
+      })
+}
+</script>
