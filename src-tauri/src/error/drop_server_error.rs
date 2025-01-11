b454f13f6d305535efbb9fd68636c2db0d28c48f
quexeky <git@quexeky.dev>
2025-01-11 11:25:26 +1100

    chore(errors): Added drop server error
    Signed-off-by: quexeky <git@quexeky.dev>

---
+++ src-tauri/src/error/drop_server_error.rs
@@ -0,0 +1,10 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct DropServerError {
+    pub status_code: usize,
+    pub status_message: String,
+    pub message: String,
+    pub url: String,
+}
