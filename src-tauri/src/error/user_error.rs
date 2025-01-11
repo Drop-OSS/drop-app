a03e935b59b96102fcfa35aadc3aa863ac00c03c
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2025-01-11 12:38:36 +1100

    chore(errors): Implementing Try and FromResidual for UserValue
    Signed-off-by: quexeky <git@quexeky.dev>

--- src-tauri/src/error/user_error.rs
+++ src-tauri/src/error/user_error.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, ops::{FromResidual, Try}};
 
 use serde::Serialize;
 
@@ -28,5 +28,25 @@ impl<T: Serialize, D: Display> From<Result<T, D>> for UserValue<T, D> {
             Ok(data) => UserValue::Ok(data),
             Err(data) => UserValue::Err(data),
         }
+    }
+}
+
+impl<T: Serialize, D: Display> Try for UserValue<T, D> {
+    type Output = T;
+
+    type Residual = D;
+
+    fn from_output(output: Self::Output) -> Self {
+        Self::Ok(output)
+    }
+
+    fn branch(self) -> std::ops::ControlFlow<Self::Residual, Self::Output> {
+        todo!()
+    }
+}
+
+impl<T: Serialize, D: Display> FromResidual for UserValue<T, D> {
+    fn from_residual(residual: <Self as std::ops::Try>::Residual) -> Self {
+        todo!()
     }
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(try_trait_v2)]
+
 mod database;
 mod games;
 
