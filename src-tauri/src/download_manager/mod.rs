c9e1ed78eb77bbb3936fc8abf5ced454926207cb
quexeky <git@quexeky.dev>
2025-05-28 09:18:33 +1000

    refactor: Delete downloadable_metadata.rs
    Signed-off-by: quexeky <git@quexeky.dev>

--- src-tauri/src/download_manager/mod.rs
+++ src-tauri/src/download_manager/mod.rs
@@ -3,7 +3,6 @@ pub mod download_manager;
 pub mod download_manager_builder;
 pub mod download_thread_control_flag;
 pub mod downloadable;
-pub mod downloadable_metadata;
 pub mod internal_error;
 pub mod progress_object;
 pub mod queue;
