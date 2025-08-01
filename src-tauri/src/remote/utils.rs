1fe8edcb75f86adfe0da816d5c395e8ade4cad50
quexeky <git@quexeky.dev>
2025-08-02 09:17:47 +1000

    feat: Add get_client function
    Signed-off-by: quexeky <git@quexeky.dev>

--- src-tauri/src/remote/utils.rs
+++ src-tauri/src/remote/utils.rs
@@ -1,12 +1,18 @@
-use std::sync::Mutex;
+use std::{
+    fs::{self, File},
+    io::Read,
+    sync::Mutex,
+};
 
 use log::{debug, warn};
+use reqwest::Certificate;
 use serde::Deserialize;
 use url::Url;
 
 use crate::{
-    database::db::borrow_db_mut_checked, error::remote_access_error::RemoteAccessError, AppState,
-    AppStatus,
+    AppState, AppStatus,
+    database::db::{DATA_ROOT_DIR, borrow_db_mut_checked},
+    error::remote_access_error::RemoteAccessError,
 };
 
 #[derive(Deserialize)]
@@ -15,6 +21,35 @@ struct DropHealthcheck {
     app_name: String,
 }
 
+pub fn get_client() -> reqwest::blocking::ClientBuilder {
+    let mut client = reqwest::blocking::ClientBuilder::new();
+    let certificate_dir = DATA_ROOT_DIR.join("certificates");
+
+    let mut certs = Vec::new();
+    match fs::read_dir(certificate_dir) {
+        Ok(c) => {
+            for entry in c {
+                match entry {
+                    Ok(c) => {
+                        let mut buf = Vec::new();
+                        File::open(c.path()).unwrap().read_to_end(&mut buf).unwrap();
+                        for cert in Certificate::from_pem_bundle(&buf).unwrap() {
+                            certs.push(cert);
+                        }
+                    }
+                    Err(_) => todo!(),
+                }
+            }
+        }
+        Err(e) => {
+            debug!("Not loading certificates due to error {e}");
+        }
+    };
+    for cert in certs {
+        client = client.add_root_certificate(cert);
+    }
+    return client;
+}
 pub fn use_remote_logic(
     url: String,
     state: tauri::State<'_, Mutex<AppState<'_>>>,
