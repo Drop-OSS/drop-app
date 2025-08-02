aa034c21a975e8024ad69c47c23e9edf30b5cdb4
Beethoven <fleskywood@gmail.com>
2025-08-02 18:05:22 -0500

    (build_appimage.sh) add basic arch detection

--- build_appimage.sh
+++ build_appimage.sh
@@ -2,20 +2,27 @@
 
 # run this from the root of the git repo to make this work
 
+arch="$(uname -m)"
 git_dir="./"
 target_dir="$git_dir/src-tauri/target/"
 appimage_dir="$git_dir/build/appimage"
 appdir="$appimage_dir/drop-oss-app.d"
 
+build() {
+	# set up the repo
+	git submodule init
+	git submodule update --recursive
+
+	# set up yarn and build
+	yarn
+	yarn tauri build
+}
+
 rm -f $appdir/usr/bin/* $appdir/usr/lib/*
 
-# set up the repo
-git submodule init
-git submodule update --recursive
-
-# set up yarn and build
-yarn
-yarn tauri build
+if [[ ! "$1" == "--nobuild" ]]; then
+	build	
+fi
 
 # install binaries in the appdir, then the libraries
 cp $target_dir/release/drop-app $appdir/usr/bin
@@ -24,7 +31,7 @@ do
 	sudo install -g $USER -o $USER -Dm755 "$(ls -L1 /usr/lib/$i)" $appdir/usr/lib
 done
 
-wget -O $appimage_dir/appimagetool https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+wget -O $appimage_dir/appimagetool https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$arch.AppImage
 
 cd $appimage_dir
 chmod u+x appimagetool
