0b87eae1b60512ee3dd56682d8eb3489f79f0f2a
DecDuck <declanahofmeyr@gmail.com>
2025-08-15 23:39:47 +1000

    fix: windows build again

--- src-tauri/src/process/process_manager.rs
+++ src-tauri/src/process/process_manager.rs
@@ -346,10 +346,12 @@ impl ProcessManager<'_> {
             .to_string();
 
         #[cfg(target_os = "windows")]
+        use std::os::windows::process::CommandExt;
+        #[cfg(target_os = "windows")]
         let mut command = Command::new("cmd");
         #[cfg(target_os = "windows")]
         command.raw_arg(format!("/C \"{}\"", &launch_string));
-        
+
         info!("launching (in {install_dir}): {launch_string}",);
 
         #[cfg(unix)]
