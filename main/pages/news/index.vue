96df57ac547a6a56fffc00e7f5bb3edcfa997d48
quexeky <git@quexeky.dev>
2025-08-19 12:17:41 +1000

    chore: Add "Under Construction" page for community and news
    Signed-off-by: quexeky <git@quexeky.dev>

---
+++ main/pages/news/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mx-auto flex flex-col items-center gap-y-4 max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div>
+            <Wordmark />
+        </div>
+        <div class="text-center">
+            <h1 class="text-balance text-4xl font-bold font-display tracking-tight text-zinc-100 sm:text-6xl">
+                Under construction
+            </h1>
+            <p class="mt-6 text-lg leading-8 text-zinc-400">
+                Yes, we know. We're working on it <a class="text-white" target="_blank"
+                    href="https://github.com/Drop-OSS/drop-app/issues/52">here.</a>
+            </p>
+        </div>
+    </div>
+</template>
