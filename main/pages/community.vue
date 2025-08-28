b808bd7e7848855a25f664847c95040c25d1efe1
DecDuck <declanahofmeyr@gmail.com>
2025-08-28 18:18:01 +1000

    feat: add placeholders for community & news pages

---
+++ main/pages/community.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grow w-full h-full flex items-center justify-center">
+    <div class="flex flex-col items-center">
+      <WrenchScrewdriverIcon
+        class="h-12 w-12 text-blue-600"
+        aria-hidden="true"
+      />
+      <div class="mt-3 text-center sm:mt-5">
+        <h1 class="text-3xl font-semibold font-display leading-6 text-zinc-100">
+          Under construction
+        </h1>
+        <div class="mt-4">
+          <p class="text-sm text-zinc-400 max-w-lg">
+            This page hasn't been implemented yet.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {
+  WrenchScrewdriverIcon,
+} from "@heroicons/vue/20/solid";
+</script>
